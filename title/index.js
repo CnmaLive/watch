a0db11210b048ed369be4421ba117700c465120a
TheBestThereEvrWas <livedragon2005@gmail.com>
2023-12-25 22:19:25 +0200

    Update index.js

--- title/index.js
+++ title/index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
     let id = b.substring(b.indexOf("=") + 1);
 
     let index = b.charAt(0);
-    var source = `https://vidsrc.me/embed/tv?tmdb=${id}/1/1`;
+    var source = `https://vidsrc.me/embed/tv?tmdb=${id}/1/2`;
 
     if(index == "m"){
       source = `https://vidsrc.me/embed/movie?tmdb=${id}`;
