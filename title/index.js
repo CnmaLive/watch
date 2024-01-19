6ee86e1777f35284cf339cb6b770171786eaeb53
TheBestThereEvrWas <livedragon2005@gmail.com>
2024-01-19 22:15:18 +0200

    test mobile resize

--- title/index.js
+++ title/index.js
@@ -64,6 +64,12 @@ document.addEventListener("DOMContentLoaded", function(){
 
   container.appendChild(iframe);
 
+  // Moblie resize:
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 });
 
 // Save Season & Episode
